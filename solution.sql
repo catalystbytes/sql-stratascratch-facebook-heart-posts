059c4463b60fb5787aecea7a94889fd6fcec1aea
catalystbytes <153075269+catalystbytes@users.noreply.github.com>
2026-04-27 17:22:24 +0800

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -1,10 +1,11 @@
-select fp.post_id,
-fp.poster,
-fp.post_text,
-fp.post_keywords,
-fp.post_date
+SELECT
+    fp.post_id,
+    fp.poster,
+    fp.post_text,
+    fp.post_keywords,
+    fp.post_date
 
-from facebook_posts fp
+FROM facebook_posts fp
 
-join (select post_id from facebook_reactions where reaction = 'heart' group by post_id) fr
+JOIN (select post_id from facebook_reactions where reaction = 'heart' group by post_id) fr
     on fp.post_id = fr.post_id;
